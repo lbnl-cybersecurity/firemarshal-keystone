c0ac23919e0d0d5f6a31532f3bf262fa5545537f
Farzad Fatollahi-Fard <ffard@lbl.gov>
2024-12-02 12:37:19 -0800

    x

--- source-keystone.sh
+++ source-keystone.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ -z "$FIRESIM_SOURCED"]]; then
+if [[ -z "${FIRESIM_SOURCED}" ]]; then
     echo "\$FIRESIM_SOURCED is not set!"
     echo "Make sure to source FireSim before proceeding with FireMarshal Keystone"
     exit 1
