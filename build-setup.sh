9e0e4310d9a866c3f9f880a106807859b030860c
Farzad Fatollahi-Fard <ffard@lbl.gov>
2024-12-02 12:32:00 -0800

    Moved git config steps to build-setup.sh

--- build-setup.sh
+++ build-setup.sh
@@ -3,3 +3,7 @@
 sudo apt update && sudo apt install pkg-config-riscv64-linux-gnu
 
 conda env create -f keystone-reqs.yml
+
+git config --global http.version HTTP/1.1
+git config --global http.postBuffer 524288000
+git config --global core.compression 0
